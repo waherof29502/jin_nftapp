35e042921ac3e932f8a226f4b2afcd2cc5ce90be
JinPing <39645652+waherof29502@users.noreply.github.com>
2022-07-03 21:06:39 +0800

    finished code

--- App.js
+++ App.js
@@ -1,9 +1,14 @@
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
+import { LogBox } from 'react-native';
 
 import Home from './screens/Home';
 import Details from './screens/Details';
+
+LogBox.ignoreLogs([
+  "[react-native-gesture-handler] Seems like you're using an old API with gesture components, check out new Gestures system!",
+]);
 
 const Stack = createStackNavigator();
 
@@ -16,6 +21,15 @@ const theme = {
 };
 
 const App = () => {
+  const [loaded] = useFonts({
+    InterBold: require('./assets/fonts/Inter-Bold.ttf'),
+    InterSemiBold: require('./assets/fonts/Inter-SemiBold.ttf'),
+    InterMedium: require('./assets/fonts/Inter-Medium.ttf'),
+    InterRegular: require('./assets/fonts/Inter-Regular.ttf'),
+    InterLight: require('./assets/fonts/Inter-Light.ttf'),
+  });
+
+  if (!loaded) return null;
   return (
     <NavigationContainer theme={theme}>
       <Stack.Navigator
